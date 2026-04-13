68c2ef4d000a4afe58cefa4077a49a0d11be2915
dekamiles23 <skarking27@gmail.com>
2026-04-12 21:20:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 const transition = document.querySelector(".transition");
 
+// fade ao entrar (fade IN)
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        transition.classList.remove("active");
+    }, 100);
+});
+
+// fade ao sair
 document.querySelectorAll("a").forEach(link => {
     link.addEventListener("click", function(e) {
 
-        e.preventDefault(); // bloqueia SEMPRE
-
         const href = this.getAttribute("href");
+
+        if (!href || href.startsWith("http")) return;
+
+        e.preventDefault();
 
         transition.classList.add("active");
 
         setTimeout(() => {
             window.location.href = href;
-        }, 600);
+        }, 500);
     });
 });
