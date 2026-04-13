5efc109669b39281a8468cbf2206b2fdc32e6e35
dekamiles23 <skarking27@gmail.com>
2026-04-12 21:12:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-document.body.onclick = () => {
-    document.querySelector(".transition").classList.add("active");
-}
+const transition = document.querySelector(".transition");
+
+document.querySelectorAll("a").forEach(link => {
+    link.addEventListener("click", function(e) {
+
+        e.preventDefault(); // bloqueia SEMPRE
+
+        const href = this.getAttribute("href");
+
+        transition.classList.add("active");
+
+        setTimeout(() => {
+            window.location.href = href;
+        }, 600);
+    });
+});
