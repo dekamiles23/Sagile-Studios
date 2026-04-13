c985146eb48f6a84a23a30b05dc92ea7333d8989
dekamiles23 <skarking27@gmail.com>
2026-04-12 21:12:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-const transition = document.querySelector(".transition");
-
-document.querySelectorAll("a").forEach(link => {
-    link.addEventListener("click", function(e) {
-
-        const href = this.getAttribute("href");
-
-        // ignora links externos ou vazios
-        if (!href || href.startsWith("http")) return;
-
-        e.preventDefault();
-
-        transition.classList.add("active");
-
-        setTimeout(() => {
-            window.location.href = href;
-        }, 500); // tempo do fade
-    });
-});
-
-// fade ao entrar na página
-window.addEventListener("load", () => {
-    transition.classList.remove("active");
-});
+document.body.onclick = () => {
+    document.querySelector(".transition").classList.add("active");
+}
