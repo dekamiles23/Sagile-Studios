db405936e70091217eb89895ef870cc0d61c5f1a
dekamiles23 <skarking27@gmail.com>
2026-04-13 12:02:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    const searchInput = document.getElementById("search");
+    const cards = document.querySelectorAll(".card");
+
+    function filtrar() {
+        const valor = searchInput.value.toLowerCase();
+
+        cards.forEach(card => {
+            const nome = card.dataset.nome.toLowerCase();
+
+            if (nome.includes(valor)) {
+                card.style.display = "";
+            } else {
+                card.style.display = "none";
+            }
+        });
+    }
+
+    // 🔍 enquanto digita
+    searchInput.addEventListener("input", filtrar);
+
+    // ⌨️ quando aperta ENTER
+    searchInput.addEventListener("keydown", (e) => {
+        if (e.key === "Enter") {
+            e.preventDefault(); // evita reload
+            filtrar();
+        }
+    });
+
+});
